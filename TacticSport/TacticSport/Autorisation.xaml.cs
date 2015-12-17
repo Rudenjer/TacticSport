193c2f30b9d6c4bebd0d9db9a3f46c2514fe60b6
Rudenjer <denjers@yandex.ru>
2015-12-17 22:37:55 +0200

    Последняя

---
+++ TacticSport/TacticSport/Autorisation.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TacticSport
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class Autorisation : Window
+    {
+        public Autorisation()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonReg_Click(object sender, RoutedEventArgs e)
+        {
+            var reg = new Registration();
+            reg.ShowDialog();
+        }
+
+        private void ButtonSign_Click(object sender, RoutedEventArgs e)
+        {
+            var main = new Main();
+            main.ShowDialog();
+        }
+    }
+}
